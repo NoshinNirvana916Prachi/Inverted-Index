2124f561f23a4c31d2850b2609f2ebde6d9a41d3
Noshin Nirvana Prachi <58959257+NoshinNirvana916Prachi@users.noreply.github.com>
2024-03-04 04:26:34 -0500

    Add files via upload

---
+++ InvertedIndex_ForkJoinPool_Executors/src/main/java/client/InvertedIndexClient.java
@@ -0,0 +1,22 @@
+package client;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.List;
+import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+import service.InvertedIndexService;
+
+public class InvertedIndexClient {
+    public static void main(String[] args) {
+        try {
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
